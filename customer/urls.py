597a34828d113b67b50d2e7537829555953f197d
DostonTogaev <togayevd37@gmail.com>
2024-07-10 02:01:19 +0500

    first commit

---
+++ customer/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path
+
+from customer.views.auth import login_page, logout_page, register_page
+from customer.views.customers import customers, add_customer, delete_customer, edit_customer, customer_detail
+
+urlpatterns = [
+    path('customer-list/', customers, name='customers'),
+    path('customer-detail/<int:customer_id>', customer_detail, name='customer-detail'),
+    path('add-customer/', add_customer, name='add_customer'),
+    path('customer/<int:pk>/delete', delete_customer, name='delete'),
+    path('customer/<int:pk>/update', edit_customer, name='edit'),
+    # Authentication path
+    path('login-page/', login_page, name='login'),
+    path('logout-page/', logout_page, name='logout'),
+    path('register-page/', register_page, name='register'),
+]
